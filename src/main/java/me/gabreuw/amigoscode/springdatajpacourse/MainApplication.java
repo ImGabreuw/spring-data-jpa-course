f6abbe54a3594288b3fad0fd3d7957e4f8983991
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-29 19:06:58 -0300

    Project created

---
+++ src/main/java/me/gabreuw/amigoscode/springdatajpacourse/MainApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.amigoscode.springdatajpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
