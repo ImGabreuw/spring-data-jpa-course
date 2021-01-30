207ac0e5324564cd71c3aa0f2a46757ccc4a6f30
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-30 10:33:18 -0300

    Column annotation

--- src/main/java/me/gabreuw/amigoscode/springdatajpacourse/model/entities/Student.java
+++ src/main/java/me/gabreuw/amigoscode/springdatajpacourse/model/entities/Student.java
@@ -18,10 +18,36 @@ public class Student {
             strategy = SEQUENCE,
             generator = "student_sequence"
     )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+    )
     private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
     private Integer age;
 
     public Student() {
