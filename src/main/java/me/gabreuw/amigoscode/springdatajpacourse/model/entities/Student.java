4b531031de089f41acc55dfdc12f0de3e3577bb3
ImGabreuw <gabrielkazama9@gmail.com>
2021-01-30 10:25:40 -0300

    SequenceGenerator/GeneratedValue annotation

--- src/main/java/me/gabreuw/amigoscode/springdatajpacourse/model/entities/Student.java
+++ src/main/java/me/gabreuw/amigoscode/springdatajpacourse/model/entities/Student.java
@@ -1,13 +1,23 @@
 package me.gabreuw.amigoscode.springdatajpacourse.model.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity(name = "Student")
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
